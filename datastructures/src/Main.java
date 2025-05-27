1e559c0a423b46c78d518a5473176dd3c1fba112
Daniel Naiff <danielnaiff344@gmail.com>
2025-05-27 10:38:26 -0300

    set

--- datastructures/src/Main.java
+++ datastructures/src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(0);
-
-        myLinkedList.append(1);
-        myLinkedList.append(2);
-        myLinkedList.append(3);
+        LinkedList myLinkedList = new LinkedList(11);
+        myLinkedList.append(4);
+        myLinkedList.append(23);
+        myLinkedList.append(7);
+        myLinkedList.set(1,3);
 
 //        System.out.println(myLinkedList.removeLast());
 //        System.out.println(myLinkedList.removeLast());
 //        System.out.println(myLinkedList.removeLast());
 
-        System.out.println(myLinkedList.get(2).value + "\n");
+
         myLinkedList.printList();
     }
 }
