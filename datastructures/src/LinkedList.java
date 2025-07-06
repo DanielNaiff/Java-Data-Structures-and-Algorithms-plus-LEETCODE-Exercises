6892e21cbe4c5454872a987f77935d373b4446c3
Daniel Naiff <danielnaiff344@gmail.com>
2025-07-06 17:16:43 -0300

    reverse

--- datastructures/src/LinkedList.java
+++ datastructures/src/LinkedList.java
@@ -32,6 +32,22 @@ public class LinkedList {
         }
     }
 
+    public void reverse(){
+        Node temp = head;
+        head = tail;
+        tail = temp;
+
+        Node after = temp.next;
+        Node before = null;
+
+        for(int i = 0; i < length; i++){
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
+    }
+
     // Adiciona um novo nó ao final da lista
     public void append(int value){
         Node newNode = new Node(value);
--- datastructures/src/Main.java
+++ datastructures/src/Main.java
@@ -2,14 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         LinkedList myLinkedList = new LinkedList(11);
+        myLinkedList.append(2);
         myLinkedList.append(3);
-        myLinkedList.append(23);
-        myLinkedList.append(7);
+        myLinkedList.append(4);
 
         myLinkedList.printList();
 
-        myLinkedList.remove(2);
-
+        myLinkedList.reverse();
         myLinkedList.printList();
 
 //        System.out.println(myLinkedList.removeLast());
